82ab06cd0eda711457416ea0117ebb563ab88c75
Kyle Gensheimer <kylegensheimer@gmail.com>
2020-02-04 16:14:44 -0500

    calendar and sample list

---
+++ routes/addcase.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+var stylesheets = [
+    { stylesheet: '/stylesheets/simple-sidebar.css' }];
+
+/* GET admin page */
+router.get('/', function(req, res, next) {
+  res.render('addcase', {
+    stylesheets: stylesheets
+  });
+});
+
+module.exports = router;
