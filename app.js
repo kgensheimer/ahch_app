cf3b58c497ad36a64bfc8e935efde6a0abbd771e
Kyle Gensheimer <kylegensheimer@gmail.com>
2020-02-11 15:04:39 -0500

    cases page added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 var addcaseRouter = require('./routes/addcase');
+var casesRouter = require('./routes/cases');
 
 var app = express();
 
@@ -25,7 +26,8 @@ app.use('/modules', express.static(path.join(__dirname, 'node_modules')))
 
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
-app.use('/addcase', addcaseRouter)
+app.use('/addcase', addcaseRouter);
+app.use('/cases', casesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
