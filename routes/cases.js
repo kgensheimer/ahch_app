178646f3ff111418a82f46a328d727c8f8028cab
Kyle Gensheimer <kylegensheimer@gmail.com>
2020-02-13 15:05:18 -0500

    comments added for clarity

--- routes/cases.js
+++ routes/cases.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET admin page */
+/* GET cases page */
 router.get('/', function(req, res, next) {
   res.render('cases', 
   {
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+// scripts sent to layout.hbs (lines 13-15)
 var scripts = [
   { script: 'modules/@fullcalendar/core/main.min.js' },
   { script: 'modules/@fullcalendar/daygrid/main.min.js' },
   { script: 'javascripts/calendar.js' }];
 
+// stylesheets sent to layout.hbs (lines 71-72)
 var stylesheets = [
   { stylesheet: 'modules/@fullcalendar/core/main.min.css' },
   { stylesheet: 'modules/@fullcalendar/daygrid/main.min.css' }]
