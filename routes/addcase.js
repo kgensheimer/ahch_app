ce042f3676336ca0c1b40400355dc928e077501b
Kyle Gensheimer <kylegensheimer@gmail.com>
2020-02-20 15:48:00 -0500

    added sidebar

--- routes/addcase.js
+++ routes/addcase.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var stylesheets = [
-    { stylesheet: '/stylesheets/simple-sidebar.css' }];
+  { stylesheet: '/stylesheets/simple-sidebar.css' }];
 
 /* GET admin page */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('addcase', {
     stylesheets: stylesheets
   });
--- routes/cases.js
+++ routes/cases.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var stylesheets = [
+  { stylesheet: '/stylesheets/simple-sidebar.css' }];
+
 /* GET admin page */
-router.get('/', function(req, res, next) {
-  res.render('cases', 
-  {
-      title: "cases page!!!"
+router.get('/', function (req, res, next) {
+  res.render('cases', {
+    stylesheets: stylesheets
   });
 });
 
